5ddaf236b0937a8b048dad76edf09c154bd579e6
RuneScaping <runescapop@proton.me>
2022-07-31 06:05:14 +0000

    removing unused variables

--- brain_tumor_segmentation_cnn/brain_tumor_segmentation_models.py
+++ brain_tumor_segmentation_cnn/brain_tumor_segmentation_models.py
@@ -1,3 +1,7 @@
 """
 
-The following code try to implement the CNN model described 
+The following code try to implement the CNN model described in
+(S. Pereira et al.)  ( http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7426413&isnumber=7463083)
+It consist mainly of a class for compiling/loading, fitting and saving two kind of models,
+and saving the processed group of images used for test.
+The segmentation is described i
