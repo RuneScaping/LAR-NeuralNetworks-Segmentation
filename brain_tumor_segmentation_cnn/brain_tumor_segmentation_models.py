9251452e95686d626c4011205da24da390ef6436
RuneScaping <runescapop@proton.me>
2022-07-31 06:58:24 +0000

    Update landing-page.css

--- brain_tumor_segmentation_cnn/brain_tumor_segmentation_models.py
+++ brain_tumor_segmentation_cnn/brain_tumor_segmentation_models.py
@@ -4,4 +4,13 @@ The following code try to implement the CNN model described in
 (S. Pereira et al.)  ( http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7426413&isnumber=7463083)
 It consist mainly of a class for compiling/loading, fitting and saving two kind of models,
 and saving the processed group of images used for test.
-The segmentation is described i
+The segmentation is described in accordance to the rules of the Brats Contest.
+
+"""
+
+from __future__ import print_function
+from skimage.color import rgb2gray
+from glob import glob
+from skimage import io, color, img_as_float
+from skimage.exposure import adjust_gamma
+from sklearn.feature_extraction.image import extract_patches_2d
