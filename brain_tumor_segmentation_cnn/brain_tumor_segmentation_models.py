13d1ea160a62c6b4babf036213a96345742708c5
RuneScaping <runescapop@proton.me>
2022-09-25 17:21:46 +0000

    Look and feel v12

--- brain_tumor_segmentation_cnn/brain_tumor_segmentation_models.py
+++ brain_tumor_segmentation_cnn/brain_tumor_segmentation_models.py
@@ -66,4 +66,9 @@ class Brain_tumor_segmentation_model(object):
         """
 
         :param model_name: if loaded_model is True load the model name specified
-  
+        :param is_hgg: if True compile model for HGG if False for LGG
+        :param n_chan:number of channels being assessed. defaults to 4
+        :param loaded_model: True if loading a pre-existing model. defaults to False
+        """
+        self.n_chan = n_chan
+        self
