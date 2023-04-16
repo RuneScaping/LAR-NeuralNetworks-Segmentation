619374b89ad7c402d7efd7782461c547ca2ea6de
RuneScaping <runescapop@proton.me>
2023-04-16 19:38:09 +0000

    // commit

--- pre_processing/n4_bias_correction.py
+++ pre_processing/n4_bias_correction.py
@@ -1,2 +1,13 @@
 from nipype.interfaces.ants import N4BiasFieldCorrection
-imp
+import sys
+import ast
+
+if len(sys.argv) < 2:
+    print("INPUT from ipython: run n4_bias_correction "
+          "input_image dimension n_iterations(optional,"
+          " form:[n_1,n_2,n_3,n_4]) output_image(optional)")
+    sys.exit(1)
+
+# if output_image is given
+if len(sys.argv) > 3:
+    n4 = N4BiasField
