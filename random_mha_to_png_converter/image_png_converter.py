17f9d2e89e0865f62d6c80ab2b44ddfdd862f921
RuneScaping <runescapop@proton.me>
2023-04-23 07:02:32 +0000

    Change readme

--- random_mha_to_png_converter/image_png_converter.py
+++ random_mha_to_png_converter/image_png_converter.py
@@ -1,4 +1,13 @@
 """
 
 
-This code 
+This code pick randomly an image between all .mha images (picked up randomly) in the specified folder and
+convert it into .png image  in accordance to the number of images required.
+
+
+"""
+
+from __future__ import print_function
+import SimpleITK.SimpleITK as sitk
+import matplotlib.pyplot as plt
+import rando
