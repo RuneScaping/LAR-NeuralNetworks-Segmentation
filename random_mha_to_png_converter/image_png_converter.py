6db8f6833393a724276ae53da05a3be6aa7d4a0f
RuneScaping <runescapop@proton.me>
2023-04-23 14:13:05 +0000

    Fixing back height

--- random_mha_to_png_converter/image_png_converter.py
+++ random_mha_to_png_converter/image_png_converter.py
@@ -10,4 +10,16 @@ convert it into .png image  in accordance to the number of images required.
 from __future__ import print_function
 import SimpleITK.SimpleITK as sitk
 import matplotlib.pyplot as plt
-import rando
+import random as rnd
+import numpy as np
+from glob import glob
+from os import makedirs
+from os.path import isdir
+from errno import EEXIST
+
+__author__ = "Cesare Catavitello"
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Cesare Catavitello"
+__email__ = "cesarec88@gmail.com"
+__status__ = "Producti
