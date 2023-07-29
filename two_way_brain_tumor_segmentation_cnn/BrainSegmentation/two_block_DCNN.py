04259ca633c204a8d58fdc24e8a2ff84ede40d49
RuneScaping <runescapop@proton.me>
2023-07-29 11:16:15 +0000

    carga inicial

--- two_way_brain_tumor_segmentation_cnn/BrainSegmentation/two_block_DCNN.py
+++ two_way_brain_tumor_segmentation_cnn/BrainSegmentation/two_block_DCNN.py
@@ -1,4 +1,18 @@
 import numpy as np
 from keras.models import Model
 from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers im
+from keras.layers import Dropout, Input
+from keras.layers.merge import Concatenate
+from keras.optimizers import SGD
+from keras import regularizers
+from keras.constraints import max_norm
+
+__author__ = "Matteo Causio"
+
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Matteo Causio"
+__status__ = "Production"
+
+
+class T
