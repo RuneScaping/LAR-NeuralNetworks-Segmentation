950797b788896426646de3c2b4ed0542c8fc745b
RuneScaping <runescapop@proton.me>
2023-07-30 10:45:45 +0000

    cr

--- two_way_brain_tumor_segmentation_cnn/BrainSegmentation/two_block_DCNN.py
+++ two_way_brain_tumor_segmentation_cnn/BrainSegmentation/two_block_DCNN.py
@@ -25,4 +25,15 @@ class TwoBlocksDCNN(object):
         self.learning_rate = learning_rate
         self.momentum_rate = momentum_rate
         self.decay_rate = decay_rate
-        self.l1_rat
+        self.l1_rate = l1_rate
+        self.l2_rate = l2_rate
+        self.model = self.twoBlocksDCNN()
+
+    # model of TwoPathCNN
+    def twoBlocksDCNN(self):
+        """
+
+
+        :param in_channels: int, number of input channel
+        :param in_shape: int, dim of the input image
+        :return: Model, 
